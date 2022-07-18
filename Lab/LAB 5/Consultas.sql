2e390c3b2d90c74d18dc46412f107c3db84dcbb7
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-07-17 23:42:48 -0500

    Se agrega las consultas de la act. 3

--- Lab/LAB 5/Consultas.sql
+++ Lab/LAB 5/Consultas.sql
@@ -1,4 +1,5 @@
-
+;---------------------------------------------------------------------
+;-------------- ACTIVIDAD 2:
 ;-- Ejercicio 1. ¿Cuántos ríos fluyen a través 
 ;-- de cada país que tiene más de 15 ríos?
 
@@ -8,6 +9,7 @@ WHERE Country.code = geo_river.country
 GROUP BY Country.name
 HAVING COUNT(*) > 15;
 
+;---------------------------------------------------------------------
 ;-- Ejercicio 2. Obtenga el nombre de los países que 
 ;-- pertenecen o son miembros de la UNESCO o de UNASUR.
 
@@ -24,8 +26,8 @@ SELECT *
 FROM ismember 
 WHERE  organization = 'UNESCO' or organization = 'UNASUR';
 
-
-;-- Ejercicio No. 3: ¿Cuál es el nombre, población y altitud 
+;---------------------------------------------------------------------
+;------- Ejercicio No. 3: ¿Cuál es el nombre, población y altitud 
 ;-- de cada ciudad en Colombia(según lo registrado en la base de datos)? 
 ;-- Los resultados deben estar ordenados de manera descendente por población.
 
@@ -34,10 +36,9 @@ FROM City JOIN Country ON City.country = Country.code
 WHERE Country.name = 'Colombia'
 ORDER BY population DESC;
 
-
-;-- Ejercicio 4.a) el PIB está compuesto al menos en un 70%
-;-- por los sectores de Servicios e Industria juntos
-     
+;---------------------------------------------------------------------
+;----- Ejercicio 4.a) el PIB está compuesto al menos en un 70%
+;-- por los sectores de Servicios e Industria juntos     
 ;-- (b) la inflación es inferior al 2%.
         
 ;-- Realizando la consulta con las dos condiciones 4a y 4b
@@ -54,8 +55,57 @@ SELECT Country.name
 FROM Country INNER JOIN Economy ON Country.code = Economy.country
 WHERE (economy.service +economy.industry)>=70 or economy.inflation < 2 ;
 
-;--------------------------------------------------------------------
+;---------------------------------------------------------------------
+;-------------- ACTIVIDAD 3:
 
+;-- Ejercicio 1:
+
+;-- Enunciado:
+SELECT CONCAT(c.Code, “ ”, c.Name) AS “Country Name”,
+FROM Country c, Economy e
+WHERE e.Unemployment >= 15
+
+
+;-- Respuesta:
+
+SELECT CONCAT(c.Code, ' ', c.Name) AS "Country Name"
+FROM Country c, Economy e
+WHERE e.Unemployment >= 15
+
+;-- Ejercicio 2
+
+;-- Enunciado:
+
+SELECT *
+FROM desert 
+ORDER BY Area DESC, Name ASC 
+
+
+;-- Respuesta:
+
+SELECT * 
+FROM desert 
+ORDER BY Area DESC 
+
+
+;-- Ejercicio 3:
+
+;-- Enunciado:
+
+SELECT name FROM Country c
+JOIN isMember m ON c.code = m.country
+WHERE m.organization = 'Mercosur'
+
+
+;-- Resultado:
+
+SELECT name
+     FROM Country as C
+     WHERE C.code IN (SELECT country
+               FROM ismember as m
+               WHERE m.organization = 'Mercosur' );
+
+;--------------------------------------------------------------------
 
 ;-- PRUEBAS:
 
@@ -89,4 +139,6 @@ FROM Country
 WHERE code IN (SELECT country
                FROM Economy
                WHERE  inflation < 2 );
+
+
 
